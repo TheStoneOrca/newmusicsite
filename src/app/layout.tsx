13a5746db3b6528976d51d249f9a815c2128bdfa
TheStoneOrca <144631439+TheStoneOrca@users.noreply.github.com>
2024-02-22 22:14:02 -0500

    Homepage started, lookin' good

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
