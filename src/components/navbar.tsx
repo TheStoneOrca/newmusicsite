90551e3e172d01fff6919ed99c9ce3f1d4a4370d
TheStoneOrca <144631439+TheStoneOrca@users.noreply.github.com>
2024-04-06 18:07:55 -0400

    Added navbar rendering and making admin panel

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,60 +11,92 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
-import { Book, Menu, ShoppingCartIcon } from "lucide-react";
+import {
+  Book,
+  Computer,
+  Loader2Icon,
+  Menu,
+  ShoppingCartIcon,
+} from "lucide-react";
+import useUser from "@/app/hooks/getuser";
 
 export default function Navbar() {
+  const { isReady, isSignedIn, user } = useUser();
   return (
-    <div className="flex items-center w-full h-12 bg-black text-white gap-x-2">
-      <div className="p-3 ml-2">
-        <Link href="/">Orca's Music</Link>
-      </div>
-      <div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="flex gap-x-2 text-1xl">
-              Music <Menu />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuGroup>
-              <DropdownMenuLabel>Music Types</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem asChild>
-                <Link href="/music/orchestra">Orchestra</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/music/strings">Strings</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/music/band">Band</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/music/piano">Piano</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/music/rock">Rock</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/music/solo">Solo</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/music">All</Link>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <div className="flex justify-end">
-        <Link href="/cart">
-          <ShoppingCartIcon />
-        </Link>
-      </div>
-      <div className="flex justify-end">
-        <Link href="/library">
-          <Book />
-        </Link>
-      </div>
-    </div>
+    <>
+      {isReady ? (
+        <div className="flex items-center w-full h-12 bg-black text-white gap-x-2">
+          <div className="p-3 ml-2">
+            <Link href="/">Orca's Music</Link>
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="flex gap-x-2 text-1xl">
+                  Music <Menu />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>Music Types</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/music/orchestra">Orchestra</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/music/strings">Strings</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/music/band">Band</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/music/piano">Piano</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/music/rock">Rock</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/music/solo">Solo</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/music">All</Link>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          {isReady ? (
+            <>
+              {isSignedIn && (
+                <>
+                  <div className="flex justify-end">
+                    <Link href="/cart">
+                      <ShoppingCartIcon />
+                    </Link>
+                  </div>
+                  <div className="flex justify-end">
+                    <Link href="/library">
+                      <Book />
+                    </Link>
+                  </div>
+                  {user?.role === "admin" ||
+                    (user?.role === "owner" && (
+                      <div className="flex justify-end">
+                        <Link href="/control-panel">
+                          <Computer />
+                        </Link>
+                      </div>
+                    ))}
+                </>
+              )}
+            </>
+          ) : (
+            <Loader2Icon className="animate-spin" />
+          )}
+        </div>
+      ) : (
+        <Loader2Icon className="animate-spin" />
+      )}
+    </>
   );
 }
